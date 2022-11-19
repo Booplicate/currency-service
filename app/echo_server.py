65cc6a8c168366a01378b6dca0cc74dfb00fad61
Totally a booplicate <53382877+Booplicate@users.noreply.github.com>
2022-11-19 21:23:17 +0300

    implement echo server

---
+++ app/echo_server.py
@@ -0,0 +1,20 @@
+"""
+Module implements echo server for stdout
+"""
+
+import asyncio
+
+import app
+from . import log_utils
+
+
+async def start_server(period: int):
+    last_hash = app.app_state.get_current_hash()
+    while True:
+        new_hash = app.app_state.get_current_hash()
+        if last_hash != new_hash:
+            last_hash = new_hash
+            report = await app.app_state.generate_report()
+            log_utils.logger.info(f"Currency report:\n{report}")
+
+        await asyncio.sleep(period)
