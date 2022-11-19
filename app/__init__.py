75c5992eced79961a17a7dddb80e81b4d091aee1
Totally a booplicate <53382877+Booplicate@users.noreply.github.com>
2022-11-19 23:42:00 +0300

    add module header

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+
+from . import (
+    api,
+    currency,
+    fetcher,
+    state
+)
+
+
+__author__ = "Booplicate"
+__version__ = "0.1.0"
+
+
+# quit_event = asyncio.Event()
+app_state: state.State
